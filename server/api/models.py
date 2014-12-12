f976f8fb088fe5a6e25456c5e5e2d8c5735ebfb7
florije1988 <florije1988@gmail.com>
2014-12-12 15:13:25 +0800

    添加User模型。

--- server/api/models.py
+++ server/api/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 __author__ = 'florije'
 from . import db
+from wtforms.validators import Email
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class BaseModel(db.Model):
@@ -9,10 +11,28 @@ class BaseModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
 
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    email = db.Column(db.String(120), unique=True, nullable=False, info={'validators': Email()})
+    password = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
 class TaskModel(BaseModel):
     __tablename__ = 'tasks'
 
     title = db.Column(db.String(length=20), default='')
+    # user_id = db.Column(db.Integer, nullable=True)
     content = db.Column(db.String(length=50), default='')
 
     def __init__(self, title, content):
